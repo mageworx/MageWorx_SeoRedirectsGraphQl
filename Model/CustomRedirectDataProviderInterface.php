e79df62eb306b5c3b06e82845634a59a10148c52
Mike <m.kostevich@mageworx.com>
2023-02-20 19:18:29 +0300

    added compatibility with "route" query

--- Model/CustomRedirectDataProviderInterface.php
+++ Model/CustomRedirectDataProviderInterface.php
@@ -8,6 +8,8 @@ declare(strict_types=1);
 
 namespace MageWorx\SeoRedirectsGraphQl\Model;
 
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
 interface CustomRedirectDataProviderInterface
 {
     /**
@@ -17,4 +19,13 @@ interface CustomRedirectDataProviderInterface
      * @return array|null
      */
     public function getEntityUrlData(int $storeId, int $objectId, string $objectType): ?array;
+
+    /**
+     * @param int $storeId
+     * @param int $objectId
+     * @param string $objectType
+     * @param ResolveInfo $info
+     * @return array|null
+     */
+    public function getRouteData(int $storeId, int $objectId, string $objectType, ResolveInfo $info): ?array;
 }
